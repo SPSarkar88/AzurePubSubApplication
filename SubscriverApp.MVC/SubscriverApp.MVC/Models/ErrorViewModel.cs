5b5acefcd5e05852b263d761d4cc331d75a0566a
Subhro Prokash Sarkar <78630512+SPSarkar88@users.noreply.github.com>
2025-02-27 20:13:28 +0530

    Rearraged publisher MVC app and Added Subscriver app Mvc Project

---
+++ SubscriverApp.MVC/SubscriverApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubscriverApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
